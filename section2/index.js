954523a7213ed6460eeccd3a922bb7349a5eb023
Genaro Jose Tinoco Palacios <gtinoco@agssa.net>
2019-10-17 13:34:10 -0600

    Section 2: video 9 finished

--- section2/index.js
+++ section2/index.js
@@ -1,28 +1,43 @@
 const cluster = require('cluster');
+const morgan = require('morgan');
 
+process.env.UV_THREADPOOL_SIZE = 1;
+function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+}
 // Is the file being executed in master mode?
 if (cluster.isMaster) {
+    // If you have real multi thread cores, use this number and you shouldn't use more never
+    // Not fork more that he number of physical core that you have
     // Cause index.js to be executed *again* but
     // in slave mode
     cluster.fork();
     cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
 } else {
     // Im a child, Im going to act like a server
     // and do nothing else
+    const crypto = require('crypto');
     const express = require('express');
     const app = express();
 
-    function doWork(duration) {
-        const start = Date.now();
-        while (Date.now() - start < duration) {}
-    }
+    app.use(morgan('dev'));
 
     app.get('/', (req, res) => {
-        doWork(5000);
-        res.send('HI there');
+        crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+            res.send('HI there');
+        });
     });
     app.get('/fast', (req, res) => {
-        doWork(5000);
         res.send('This was fast');
     });
     app.listen(3000, () => {
