1708393544e4151a2f66f019d7c2d8f0b1e88362
Genaro Jose Tinoco Palacios <gtinoco@agssa.net>
2019-10-21 13:56:45 -0600

    Section 5:  Video 25

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-
+const sessionFactory = require('./factories/sessionFactory');
 let browser;
 let page;
 beforeEach(async () => {
@@ -23,12 +23,26 @@ test('The header has the correct text', async () => {
 test('clicking login starts oauth flow', async () => {
     await page.click('.right a');
 
-    const url = await  page.url();
+    const url = await page.url();
 
-    expect(url).toMatch(/accounts\.google\.com/)
-    console.log('url', url)
+    expect(url).toMatch(/accounts\.google\.com/);
+    console.log('url', url);
 });
 
+//.only ignore the rest
+test('When signed in, shows logout button', async () => {
+    const id = '5da8e2c04aea2c613f64aaaa';
+
+    const {session, sig} = sessionFactory({_id: id});
+    console.log(session, sig);
+    await page.setCookie({name: 'session', value: session});
+    await page.setCookie({name: 'session.sig', value: sig});
+    await page.goto('localhost:3000');
+
+    await page.waitFor('a');
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+    expect(text).toEqual('Logout');
+});
 afterEach(async () => {
     await browser.close();
 });
